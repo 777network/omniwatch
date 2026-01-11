a3b7c646cb86f74b6308a450519197022e13ace0
777network <simon91z@hotmail.se>
2026-01-11 15:58:55 +0100

    Set up Fastify server with legal items route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,9 @@
+import Fastify from 'fastify'
+import legalItemsRoutes from './legalItems'
 
+const app = Fastify({ logger: true })
+
+app.register(legalItemsRoutes)
+
+app.listen({ port: 3000, host: '0.0.0.0' })
+  .then(() => console.log('API running on port 3000'))
